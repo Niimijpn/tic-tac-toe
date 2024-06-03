9beff6124f1969b5bec6806419c3c1ecb6a920dc
NiimiSyunya <niimicomputergraphics@gmail.com>
2024-06-03 20:04:13 +0900

    Completing the game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,8 @@ export default function Board() {
 
   function handleClick(i: number) {
     //
+    if (squares[i] || calculateWinner(squares)) return;
+
     const nextSquares = [...squares];
     if (xIsnext) {
       nextSquares[i] = "X";
@@ -43,8 +45,41 @@ export default function Board() {
     setSquares(nextSquares);
   }
 
+  function calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [a, b, c] of lines) {
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let satus;
+  if (winner) {
+    status = "Winner:" + winner;
+  } else {
+    status = "Next player: " + (xIsnext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
